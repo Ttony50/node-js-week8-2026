6c82dee5ac2de76999512da9a2bd7992407bb2d3
Tien tony <tientony50@gmail.com>
2026-08-21 21:00:22 +0800

    feat1

--- livefit/db/migrations/1787315319190-InitLivefit.js
+++ livefit/db/migrations/1787315319190-InitLivefit.js
@@ -1,7 +1,7 @@
 /**
  * @typedef {import('typeorm').MigrationInterface} MigrationInterface
  * @typedef {import('typeorm').QueryRunner} QueryRunner
-*/
+ */
 
 /**
  * @class
@@ -13,55 +13,26 @@ module.exports = class Init1786796656379 {
     /**
      * @param {QueryRunner} queryRunner
      */
-
     async up(queryRunner) {
-  await queryRunner.query(`
-    CREATE TABLE "USER" (
-      "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-      "name" varchar(50) NOT NULL,
-      "email" varchar(320) NOT NULL,
-      "role" varchar(20) NOT NULL,
-      "created_at" timestamp NOT NULL DEFAULT now(),
-      "updated_at" timestamp NOT NULL DEFAULT now(),
-      CONSTRAINT "PK_USER" PRIMARY KEY ("id"),
-      CONSTRAINT "UQ_USER_EMAIL" UNIQUE ("email")
-    )
-  `);
+        await queryRunner.query(`CREATE TABLE "CLASS" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, CONSTRAINT "PK_f7e171cf1a8b2d418f1fd182456" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "SUBJECT" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, CONSTRAINT "PK_f4bd852a75dd2062e5d01e3d824" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "STUDENT" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, "class_id" uuid NOT NULL, CONSTRAINT "PK_85f0e0a0ccd38f6d8ea17f5e9b1" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "GRADE" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "score" integer NOT NULL, "student_id" uuid NOT NULL, "subject_id" uuid NOT NULL, CONSTRAINT "PK_bd0bb90149430debd6371dd718a" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "STUDENT" ADD CONSTRAINT "FK_4fb1ca1a74779f553a538b83e26" FOREIGN KEY ("class_id") REFERENCES "CLASS"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "GRADE" ADD CONSTRAINT "FK_92c82e6528220f8ca6b202c8fef" FOREIGN KEY ("student_id") REFERENCES "STUDENT"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "GRADE" ADD CONSTRAINT "FK_d173d08c8481c8331a3cb48b715" FOREIGN KEY ("subject_id") REFERENCES "SUBJECT"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
 
-  await queryRunner.query(`
-    CREATE TABLE "SKILL" (
-      "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-      "name" varchar(50) NOT NULL,
-      CONSTRAINT "PK_SKILL" PRIMARY KEY ("id"),
-      CONSTRAINT "UQ_SKILL_NAME" UNIQUE ("name")
-    )
-  `);
-
-  await queryRunner.query(`
-    CREATE TABLE "COURSE" (
-      "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-      "name" varchar(100) NOT NULL,
-      "description" text NOT NULL,
-      "start_at" timestamp NOT NULL,
-      "end_at" timestamp NOT NULL,
-      "max_participants" integer NOT NULL,
-      "created_at" timestamp NOT NULL DEFAULT now(),
-      "updated_at" timestamp NOT NULL DEFAULT now(),
-      "user_id" uuid NOT NULL,
-      "skill_id" uuid NOT NULL,
-      CONSTRAINT "PK_COURSE" PRIMARY KEY ("id"),
-      CONSTRAINT "FK_COURSE_USER"
-        FOREIGN KEY ("user_id") REFERENCES "USER"("id"),
-      CONSTRAINT "FK_COURSE_SKILL"
-        FOREIGN KEY ("skill_id") REFERENCES "SKILL"("id")
-    )
-  `);
-}
-
-   async down(queryRunner) {
-  await queryRunner.query(`DROP TABLE "COURSE"`);
-  await queryRunner.query(`DROP TABLE "SKILL"`);
-  await queryRunner.query(`DROP TABLE "USER"`);
-}
-
+    /**
+     * @param {QueryRunner} queryRunner
+     */
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "GRADE" DROP CONSTRAINT "FK_d173d08c8481c8331a3cb48b715"`);
+        await queryRunner.query(`ALTER TABLE "GRADE" DROP CONSTRAINT "FK_92c82e6528220f8ca6b202c8fef"`);
+        await queryRunner.query(`ALTER TABLE "STUDENT" DROP CONSTRAINT "FK_4fb1ca1a74779f553a538b83e26"`);
+        await queryRunner.query(`DROP TABLE "GRADE"`);
+        await queryRunner.query(`DROP TABLE "STUDENT"`);
+        await queryRunner.query(`DROP TABLE "SUBJECT"`);
+        await queryRunner.query(`DROP TABLE "CLASS"`);
+    }
 }
